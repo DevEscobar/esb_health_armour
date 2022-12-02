119e629c383cb904fec62d3b0d5e890404314a9e
$ESCOBAR <119685868+DevEscobar@users.noreply.github.com>
2022-12-02 16:26:56 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '$ᴇꜱᴄᴏʙᴀʀ#4685'
+description 'Ukládání životů a armouru do databáze'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_script 'client/main.lua'
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server/main.lua'
+}
+
+dependencies {
+  'oxmysql',
+  'es_extended'
+}
